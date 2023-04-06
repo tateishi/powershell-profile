3bba92707f930759d22fd20e1efa0a5e8f6b2cc3
TATEISHI Tadatoshi <ishio39@gmail.com>
2023-04-06 15:49:49 +0900

    ホームディレクトリを"~"で表示する

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
+function limit-HomeDirectory($path) {
+  $path.replace("$home", "~")
+}
+
 function prompt {
   $host.UI.write("green", $host.UI.RawUI.BackgroundColor, "PS ");
-  $host.UI.writeline("yellow", $host.UI.RawUI.BackgroundColor, (get-location).Path);
+  $host.UI.writeline("yellow", $host.UI.RawUI.BackgroundColor, $(limit-HomeDirectory("$pwd")));
   "> "
 }
 
--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,3 +1,7 @@
+function limit-HomeDirectory($path) {
+  $path.replace("$home", "~")
+}
+
 function prompt {
   $host.UI.write("green", $host.UI.RawUI.BackgroundColor, "PS ");
   $host.UI.writeline("yellow", $host.UI.RawUI.BackgroundColor, (get-location).Path);
