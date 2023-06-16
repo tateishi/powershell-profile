17f2367533c1a0fad46f42bed13964b620230c73
TATEISHI Tadatoshi <ishio39@gmail.com>
2023-06-16 11:44:16 +0900

    vscodeのpowershellのプロンプトを変更
    - ホームディレクトリを"~"に変換

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -4,7 +4,7 @@ function limit-HomeDirectory($path) {
 
 function prompt {
   $host.UI.write("green", $host.UI.RawUI.BackgroundColor, "PS ");
-  $host.UI.writeline("yellow", $host.UI.RawUI.BackgroundColor, (get-location).Path);
+  $host.UI.writeline("yellow", $host.UI.RawUI.BackgroundColor, $(limit-HomeDirectory("$pwd")));
   "> "
 }
 
