ebb74d41565f4919ca8caaaa2c22e21a5a638115
TATEISHI Tadatoshi <ishio39@gmail.com>
2025-03-12 15:24:14 +0900

    init.ps1の空白行を削除し、コードの可読性を向上

--- autoload/init.ps1
+++ autoload/init.ps1
@@ -1,17 +1,17 @@
 function limit-HomeDirectory($path) {
     $path.replace("$home", "~")
 }
-  
+
 function prompt {
     $host.UI.write("green", $host.UI.RawUI.BackgroundColor, "PS ");
     $host.UI.writeline("yellow", $host.UI.RawUI.BackgroundColor, $(limit-HomeDirectory("$pwd")));
     "> "
 }
-  
+
 function which($command) {
     get-command $command | select-object -expandproperty definition
 }
-  
+
 $env:PIPENV_VENV_IN_PROJECT="true"
 
 $paths = @(
